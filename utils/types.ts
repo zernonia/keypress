ebd2abfe6e1dfe1798f9c3b2578159aec010744c
zernonia <zernonia@gmail.com>
2022-10-08 15:29:45 +0800

    add posts

---
+++ utils/types.ts
@@ -0,0 +1,20 @@
+export interface Profiles {
+  id: string /* primary key */
+  username?: string
+  avatar_url?: string
+  name?: string
+  created_at?: string
+}
+
+export interface Posts {
+  id: string /* primary key */
+  author_id?: string /* foreign key to profiles.id */
+  created_at?: string
+  slug?: string
+  title?: string
+  body?: string
+  cover_img?: string
+  active?: boolean
+  tags?: string[]
+  profiles?: Profiles
+}
