ffdf37af425745597c73d9a3a615bbe4272b7bef
zernonia <zernonia@gmail.com>
2022-10-12 10:38:15 +0800

    add umami

---
+++ plugins/umami.client.ts
@@ -0,0 +1,24 @@
+export default defineNuxtPlugin(() => {
+  const moduleOptions = {
+    scriptUrl: "https://umami-zernonia.vercel.app/umami.js",
+    websiteId: process.env?.UMAMI_WEBSITE_ID,
+  }
+  const options = { ...moduleOptions }
+
+  if (process.env.NODE_ENV === "production" && moduleOptions.websiteId) {
+    loadScript(options)
+  }
+})
+
+function loadScript(options: any) {
+  const head = document.head || document.getElementsByTagName("head")[0]
+  const script = document.createElement("script")
+
+  script.async = true
+  script.defer = true
+  script.dataset.websiteId = options.websiteId
+  script.dataset.domains = options.domains
+  script.src = options.scriptUrl
+
+  head.appendChild(script)
+}
