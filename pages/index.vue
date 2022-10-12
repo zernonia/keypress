833a5efea5a54181c5713fc38d4648b39162b859
zernonia <zernonia@gmail.com>
2022-10-12 23:16:42 +0800

    update responsive

--- pages/index.vue
+++ pages/index.vue
@@ -53,12 +53,12 @@ watch(Slash, (n) => {
     <NuxtLink to="https://github.com/zernonia/keypress" target="_blank" class="w-full relative block">
       <NuxtImg
         style="object-position: 0 20%"
-        class="w-full h-80 object-cover rounded-2xl"
+        class="w-full h-60 md:h-80 object-cover rounded-2xl"
         src="https://images.unsplash.com/photo-1665049420194-8f562db50cbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
         alt=""
       />
       <h3
-        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl font-semibold flex flex-wrap items-center"
+        class="absolute left-4 md:left-1/2 top-8 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:text-3xl font-semibold flex flex-wrap items-center"
       >
         <div class="i-mdi-github mr-2"></div>
         Nuxt 3 + Supabase + Vercel
